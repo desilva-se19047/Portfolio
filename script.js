edece3f442b4dada34673f023b69d74d077a0a87
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-08-10 14:44:04 +0530

    create toggle function

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");   
 }
+
+const toggleElements = document.querySelectorAll('.toggle');
+
+toggleElements.forEach(toggleElement => {
+    toggleElement.addEventListener('click', () => {
+        toggleElement.classList.toggle('active');
+        const container = toggleElement.closest('.details-container');
+        const content = container.querySelector('.content');
+        container.style.height = toggleElement.classList.contains('active') ? (content.scrollHeight + 30) + 'px' : '30px';
+    });
+});

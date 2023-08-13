d027ee578f36119f3074ca8d8caa0058b4e804d9
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-08-13 17:59:41 +0530

    change media queries

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ function toggleMenu() {
 const toggleElements = document.querySelectorAll('.toggle');
 
 toggleElements.forEach(toggleElement => {
+    const container = toggleElement.closest('.details-container');
+    const content = container.querySelector('.content');
+    
+    // Hide content by default
+    content.style.display = 'none';
+    container.style.height = '30px';
+
     toggleElement.addEventListener('click', () => {
         toggleElement.classList.toggle('active');
-        const container = toggleElement.closest('.details-container');
-        const content = container.querySelector('.content');
+        content.style.display = toggleElement.classList.contains('active') ? 'block' : 'none';
         container.style.height = toggleElement.classList.contains('active') ? (content.scrollHeight + 30) + 'px' : '30px';
     });
 });
+
+
 
